657bc15730b4c6659274b2a2092305bd50d2c44c
Ha <4410618@gmail.com>
2015-11-25 01:04:15 +0900

    3D

---
+++ 3D Project/Assets/script/DamageScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageScript : MonoBehaviour {
+
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.transform.root != transform.root && col.tag != "Ground" && !col.isTrigger){
+
+            if (!col.transform.GetComponent<PlayerControll>().damage && !col.transform.GetComponent<PlayerControll>().blocking)
+            {
+                col.transform.GetComponent<PlayerControll>().damage = true;
+
+                col.transform.root.GetComponentInChildren<Animator>().SetTrigger("Damage");
+
+                col.transform.GetComponent<PlayerControll>().currentHP -= 10;
+
+                if (col.transform.GetComponent<PlayerControll>().currentHP <= 0)
+                {
+                    Destroy(col.gameObject);
+                }
+                
+            }
+        }
+    }
+}
